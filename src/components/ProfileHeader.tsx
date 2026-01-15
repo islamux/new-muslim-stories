e9bbf32c410d5e25d495d3a876d60231fd91ff24
Islamux <fathi733@gmail.com>
2026-01-15 09:15:41 +0300

    feat: temporarily hide profile photo in StoryContentDisplay
    Profile image commented out for future re-enabling

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -9,15 +9,16 @@ export default function ProfileHeader({ story }: ProfileHeaderProps) {
 
   return (
     <div className="flex items-center mb-6">
-      {story.profilePhoto && (
-        <Image
+      {/* Image temporarily hidden - to re-enable, uncomment below */}
+      {/* {story.profilePhoto && ( */}
+        {/* <Image
           src={story.profilePhoto}
           alt={story.firstName}
           width={80}
           height={80}
           className="rounded-full me-4 object-cover"
-        />
-      )}
+        /> */}
+      {/* )} */}
       <div>
         <p className="text-xl font-semibold text-gold-600">{story.firstName}</p>
         <p className="text-gray-600">
